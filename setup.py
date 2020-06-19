c7bf67cadfae6f57ad66acab1e34ff13ad5e1744
Maikel Wever <maikelwever@gmail.com>
2020-06-19 13:38:52 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+
+setup(
+    name='filedialog',
+    version='0.0.1',
+    description='A Python wrapper for opening files and folders with the native file dialog.',
+    author='Maikel Wever',
+    author_email='maikelwever@gmail.com',
+    url='https://github.com/maikelwever/filedialog/',
+    packages=['filedialog'],
+)
